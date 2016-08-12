cffe68599a3dc6c43f7572d69f0676a217e1f6cc
Vladimir Papin <vladimir.papin@gmail.com>
2016-08-13 00:25:53 +0300

    add file for encripted

--- src/main/java/ru/vladpap/plugins/ShowHelloEncrypted.java
+++ src/main/java/ru/vladpap/plugins/ShowHelloEncrypted.java
@@ -6,7 +6,7 @@ import ru.sbt.plugin.Plugin;
 public class ShowHelloEncrypted implements Plugin {
     @Override
     public void doUsefull() {
-        System.out.println("Hello World !!!");
+        System.out.println("Hello World !!!\r\nClass no encripted");
         System.out.println(this.getClass().getName() + " " + this.getClass().getClassLoader());
     }
 }
